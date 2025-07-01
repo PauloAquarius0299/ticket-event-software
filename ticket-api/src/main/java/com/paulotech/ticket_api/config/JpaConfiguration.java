b664044b4ef2beff9e4340bb1f0442682988b9a1
Paulo Cesar <paulocgsantos99@gmail.com>
2025-07-01 10:54:35 -0300

    feat: add configs

---
+++ ticket-api/src/main/java/com/paulotech/ticket_api/config/JpaConfiguration.java
@@ -0,0 +1,9 @@
+package com.paulotech.ticket_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfiguration {
+}
