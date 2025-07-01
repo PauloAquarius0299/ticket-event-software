1ef504ed821591b2d74a00ffce71ce03f3416160
Paulo Cesar <paulocgsantos99@gmail.com>
2025-07-01 08:24:25 -0300

    feat: add repository and filters user

---
+++ ticket-api/src/main/java/com/paulotech/ticket_api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.paulotech.ticket_api.repository;
+
+import com.paulotech.ticket_api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
